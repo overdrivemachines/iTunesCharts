ce301e93dc582a988f8792f19f582ded02b62c11
Dipen Chauhan <get.dipen@gmail.com>
2016-11-24 10:35:30 -0800

    Added youtube stats

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,10 @@ require 'open-uri'
 require 'json'
 
 count = 5
+youtube_api_key = "AIzaSyBCX3JOJlNp76w6b38jwgY3ksQcwHl7F70"
+entry = Hash.new
+query = ""
+youtube_result = Hash.new
 
 # Get the json from Apple
 itunes_result = JSON.parse(open("https://itunes.apple.com/us/rss/topsongs/limit=#{count}/explicit=true/json").read)
@@ -19,7 +23,7 @@ for i in 0..(count - 1)
 	entry = entries[i]
 	# Check if Song already exists
 	if (!Song.find_by_id(entry["id"]["attributes"]["im:id"].to_i))
-		# New song object
+		# New Song object
 		song = Song.new
 
 		song.id = entry["id"]["attributes"]["im:id"].to_i
@@ -31,14 +35,36 @@ for i in 0..(count - 1)
 		song.preview_audio_url = entry["link"][1]["attributes"]["href"]
 		song.release_date = entry["im:releaseDate"]["attributes"]["label"]
 
+		# Youtube
+		# Find Youtube video
+		query = entry["title"]["label"]
+		puts "Query: " + query
+		youtube_result = JSON.parse(open("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=viewCount&q=#{query}&type=video&key=#{youtube_api_key}").read) 
+		youtube_video = youtube_result["items"][0]
+		song.youtube_id = youtube_video["id"]["videoId"]		
+		puts "Youtube Video Title: " + youtube_video["snippet"]["title"]
+
+		# Get stats for the Youtube video
+		youtube_result = JSON.parse(open("https://www.googleapis.com/youtube/v3/videos?id=#{song.youtube_id}&key=#{youtube_api_key}&part=snippet,statistics").read) 
+		song.views = youtube_result["items"]["statistics"]["viewCount"]
+		puts "Views = " + song.views
+		song.likes = youtube_result["items"]["statistics"]["likeCount"]
+		puts "Likes = " + song.likes
+		song.dislikes = youtube_result["items"]["statistics"]["dislikeCount"]
+		puts "Dislikes = " + song.dislikes
+
+
 		# Check if category exists
 		if (!Category.find_by_id(song.category_id))
 			category = Category.new
 			category.id = song.category_id
 			category.name = entry["category"]["attributes"]["term"]
+
+			# Save new Category
 			category.save
 		end
 
+		# Save new Song
 		song.save
 	end
 	
