89162ab8c13a953ec5ba3c0952868a9ad1d39e18
Dipen Chauhan <get.dipen@gmail.com>
2016-11-28 18:31:46 -0800

    Added relationship

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id                :integer          not null, primary key
+#  name              :string
+#  artist            :string
+#  album             :string
+#  category_id       :integer
+#  image_url         :string
+#  preview_audio_url :string
+#  release_date      :date
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  youtube_id        :string
+#  youtube_views     :integer
+#  youtube_likes     :integer
+#  youtube_dislikes  :integer
+#
+
 class Song < ApplicationRecord
   belongs_to :category
 end
