6b2952fe0ae16890b68843a4773a8819a0691fbc
Dipen Chauhan <get.dipen@gmail.com>
2016-12-03 11:54:14 -0800

    Default for top5, top10 and top25

--- db/migrate/20161130182728_create_chart_items.rb
+++ db/migrate/20161130182728_create_chart_items.rb
@@ -3,9 +3,9 @@ class CreateChartItems < ActiveRecord::Migration[5.0]
     create_table :chart_items do |t|
       t.references :song, foreign_key: true
       t.integer :position, :limit => 1 # tinyint (1 byte, -128 to 127)
-      t.integer :top5, :limit => 2 # smallint (2 bytes, max 32,767)
-      t.integer :top10, :limit => 2 # smallint (2 bytes, max 32,767)
-      t.integer :top25, :limit => 2 # smallint (2 bytes, max 32,767)
+      t.integer :top5, :limit => 2, :default => 0 # smallint (2 bytes, max 32,767)
+      t.integer :top10, :limit => 2, :default => 0 # smallint (2 bytes, max 32,767)
+      t.integer :top25, :limit => 2, :default => 0 # smallint (2 bytes, max 32,767)
 
       t.timestamps
     end
