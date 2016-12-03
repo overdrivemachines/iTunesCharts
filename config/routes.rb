770adb5f9316e993073cdf109ff2459f5721478c
Dipen Chauhan <get.dipen@gmail.com>
2016-12-03 11:52:47 -0800

    Added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
+# == Route Map
+#
+#          Prefix Verb   URI Pattern                     Controller#Action
+#     chart_items GET    /chart_items(.:format)          chart_items#index
+#                 POST   /chart_items(.:format)          chart_items#create
+#  new_chart_item GET    /chart_items/new(.:format)      chart_items#new
+# edit_chart_item GET    /chart_items/:id/edit(.:format) chart_items#edit
+#      chart_item GET    /chart_items/:id(.:format)      chart_items#show
+#                 PATCH  /chart_items/:id(.:format)      chart_items#update
+#                 PUT    /chart_items/:id(.:format)      chart_items#update
+#                 DELETE /chart_items/:id(.:format)      chart_items#destroy
+#
+
 Rails.application.routes.draw do
   resources :chart_items
+  root "chart_items#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
